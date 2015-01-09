61d80c3eee3b64a367e96db1063f80ddbcfb7784
Alex Gryzlov <skyrodent@gmail.com>
2015-01-09 17:02:06 +0300

    add missing import

--- lzo-core/src/main/java/org/anarres/lzo/LzopOutputStream.java
+++ lzo-core/src/main/java/org/anarres/lzo/LzopOutputStream.java
@@ -45,6 +45,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.zip.Adler32;
 import java.util.zip.CRC32;
 import java.util.zip.Checksum;
